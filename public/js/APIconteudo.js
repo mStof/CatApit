8ecafad6042286d4206f3dbc440350f6d6c9519f
Mateus <137577094+mStof@users.noreply.github.com>
2023-12-12 19:21:17 -0300

    CABEIIIIIIIIII (Versão 1.0)

--- public/js/APIconteudo.js
+++ public/js/APIconteudo.js
@@ -1,14 +1,15 @@
-const pesquisaDoIndex = window.location.search.replace('?search_bar=', '')
+let pesquisaDoIndex = window.location.search.replace('?search_bar=', '')
+pesquisaDoIndex = 'rblu'
 
 const ImgsPromise = () => new Promise((resolve, reject) => {
     const xhrImgs = new XMLHttpRequest()
     xhrImgs.open('GET', `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${pesquisaDoIndex}`)
-        xhrImgs.send()
-        xhrImgs.onreadystatechange = () => {
-            if(xhrImgs.readyState === 4){     
-                return resolve(JSON.parse(xhrImgs.responseText))
-            }
+    xhrImgs.send()
+    xhrImgs.onreadystatechange = () => {
+        if(xhrImgs.readyState === 4){     
+            return resolve(JSON.parse(xhrImgs.responseText))
         }
+    }
 
 })
 const InfoPromise = () => new Promise((resolve, reject) => {
@@ -19,23 +20,22 @@ const InfoPromise = () => new Promise((resolve, reject) => {
             if(xhr.readyState === 4){
                 const breeds = JSON.parse(xhr.responseText)
                 const breed = breeds.find((breeds) => {return breeds.id == pesquisaDoIndex})
-                return resolve(breeds)
+                return resolve(breed)
             }
         }
     })
 
 const CatInfo = async () => {
     const result = await InfoPromise()
-    console.dir(result)
     const APICatName = result.name
     const APICatDescription = result.description
+    return [APICatDescription, APICatName]
 }
 const CatImgs = async () => {
     const imgs = await ImgsPromise()
     const APICatImgs = imgs.map(img => img.url)
-    console.log(APICatImgs)
+    return APICatImgs
 }
-CatImgs()
-CatInfo()
-export { CatImgs, CatInfo }
+export {CatImgs, CatInfo}
+
 
--- public/js/consteudo.js
+++ public/js/consteudo.js
@@ -1,4 +1,31 @@
-// import { CatImgs, CatInfo } from "./APIconteudo.js";
+import { CatImgs, CatInfo } from "./APIconteudo.js";
 
-// console.log(CatInfo())
-// console.log(CatImgs())
+
+
+const placeImgs = async () => {
+    const imgs = await CatImgs()
+    const container = document.querySelector('section.container')
+    const gridImgs = document.querySelectorAll('.pictures_grid img')
+
+    container.style.backgroundImage = `url(${imgs[0]})`
+    for (let i = 0; i < 10; i++) {
+        if(imgs.length > i){
+            gridImgs[i].setAttribute('src', imgs[i])
+        }
+        else{
+            gridImgs[i].setAttribute('src', '../images/Conteudo/default.jpg')
+        }
+        
+    }
+}
+const placeInfo = async () => {
+    const info = await CatInfo()
+    const description = document.querySelector('.description p') 
+    description.textContent = info[0]
+
+    const catName = Array.from(document.querySelectorAll('.change'))
+    catName.map((nome) => nome.textContent = info[1])
+
+}
+placeImgs()
+placeInfo()
