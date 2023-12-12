21cb44f527b2746a01924ec99346a3c20f238483
Mateus <137577094+mStof@users.noreply.github.com>
2023-12-11 23:17:18 -0300

    versão 1,9

--- js/conteudo.js
+++ js/conteudo.js
@@ -1,30 +1,20 @@
-const catNameNav = document.querySelector('.nav_cat_name')
-
-const catName = document.querySelector('.cat_name')
-const container = document.querySelector('.container')
-
-const attrCatName = document.querySelector('.characteristics h2')
-const catsAttr = document.querySelectorAll('.characteristic .stars')
-
-const catDescription = document.querySelector('.description p')
-
-const pictureCatName = document.querySelector('.pictures h2')
-const catsPicture = document.querySelectorAll('.pictures_grid img')
-
+const pesquisaDoIndex = 'kora'
 // doq eu preciso? nome do gato, atributos, imagens e descrição
 axios.get('https://api.thecatapi.com/v1/breeds')
     .then((result) => {
-        const breed = result.data.filter((breeds) => {return breeds.id == "kora"})[0]
+        const breed = result.data.filter((breeds) => {return breeds.id == pesquisaDoIndex})[0]
         const ApiCatName = breed.name
         const ApiCatDescription = breed.description
         
-        axios.get('https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=kora')
+        axios.get(`https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${pesquisaDoIndex}`)
         .then((imgs) => {
             const ApiGridImgs = imgs.data.map((img) => {
                 return img.url
             })
-            console.log(ApiCatName)
-            console.log(ApiCatDescription)
-            console.log(ApiGridImgs)
+            // export const Api = {
+            //     ApiCatName,
+            //     ApiCatDescription,
+            //     ApiGridImgs,
+            // }
         })
 })
