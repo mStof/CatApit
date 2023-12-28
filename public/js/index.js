55e2fbeaaded033a6a031c0e288e2af428a49daf
Mateus <137577094+mStof@users.noreply.github.com>
2023-12-28 13:50:05 -0300

    Ver 2.5
    Removendo o axios, colocando o Fetch no index.js

--- public/js/index.js
+++ public/js/index.js
@@ -2,20 +2,15 @@ let searchBar = document.querySelector('#search_bar')
 let resultBar = document.querySelector('pre.result')
 let form = document.querySelector('.search')
 
-
-axios.get('https://api.thecatapi.com/v1/breeds')
-.then((result) => {
-    console.log('carregou')
-    function getResult () {
+const myPromise = async () => {
+    try{
+        const dadosConvertidos = await fetch('https://api.thecatapi.com/v1/breeds').then( res => res.json())
+        const getResult = () => {
         resultBar.innerHTML = ``
-        const breedsFilter = result.data.filter((breeds) => {
+        const breedsFilter = dadosConvertidos.filter((breeds) => {
             let breedsName = breeds.name.toLowerCase() 
             let searchValue = searchBar.value.toLowerCase()
-            if(searchValue == ''){
-                return
-            }else if(breedsName.includes(searchValue)){
-                return breeds
-            }
+            return breedsName.includes(searchValue) && !(searchValue == '') ? breeds : false
         
         })
         for (let i = 0; i < breedsFilter.length; i++) {
@@ -27,25 +22,25 @@ axios.get('https://api.thecatapi.com/v1/breeds')
                 console.log(key)
                 if(key == ' ' || key == 'Enter'){
                     opt.click()
-                    const test = result.data.filter((e) => {return e.name == searchBar.value})
+                    const test = dadosConvertidos.filter((e) => {return e.name == searchBar.value})
                     searchBar.value = test[0].id
                     form.submit()
                 }
             })
         })
+        form.addEventListener('submit', () => {
+            
+            const test = dadosConvertidos.filter((e) => {return e.name == searchBar.value})
+            searchBar.value = test[0].id
+        })
+        }
+        return getResult
     }
-    form.addEventListener('submit', () => {
-        
-        const test = result.data.filter((e) => {return e.name == searchBar.value})
-        searchBar.value = test[0].id
-    })
-    searchBar.addEventListener('keyup', getResult)
-})
-.catch((error) => {
-    console.log(
-        `Ops! Algo deu errado =>
-        ${error}` )
-})
+    catch(error){
+        console.log(`Ops! Algo deu errado => ${error}`)
+    }
+}
+myPromise().then(getResult => searchBar.addEventListener('keyup', getResult))
 
 resultBar.addEventListener('click', (e) => {
     searchBar.value = e.target.textContent
